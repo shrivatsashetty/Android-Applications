64c1378c869e970368e0d5545ab304bcd8349aff
shrivatsashetty <shrivatsa307@gmail.com>
2024-02-08 23:19:59 +0530

    completed with building intent app

--- IntentsApp/app/src/main/java/com/shrivatsa/intentsapp/LoggedInActivity.java
+++ IntentsApp/app/src/main/java/com/shrivatsa/intentsapp/LoggedInActivity.java
@@ -2,13 +2,23 @@ package com.shrivatsa.intentsapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class LoggedInActivity extends AppCompatActivity {
 
+    TextView textView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_logged_in);
+
+        textView = findViewById(R.id.textView2);
+
+        Intent recievedIntent = getIntent(); // to fetch the incoming intent object
+        String receivedMessage = recievedIntent.getStringExtra("userMessage"); // be careful to provide the same key as an argument
+        textView.setText(receivedMessage);
+
     }
 }
--- IntentsApp/app/src/main/java/com/shrivatsa/intentsapp/MainActivity.java
+++ IntentsApp/app/src/main/java/com/shrivatsa/intentsapp/MainActivity.java
@@ -2,13 +2,38 @@ package com.shrivatsa.intentsapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    // elements declaration
+    EditText userNameHolder;
+    Button btnLogin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // typecasting elements
+        userNameHolder = findViewById(R.id.user_name_holder);
+        btnLogin = findViewById(R.id.btn_login);
+
+        // to make the cursor appear in the username box by default
+        userNameHolder.requestFocus();
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String enteredText = userNameHolder.getText().toString();
+                Intent intent = new Intent(getApplicationContext(), LoggedInActivity.class);
+                intent.putExtra("userMessage", enteredText);
+                startActivity(intent);
+            }
+        });
     }
 }
